50e25752b61a1b26b89603b3ca16270463caaa79
aytuns <ayotunde.jerry@gmail.com>
2023-08-09 00:50:35 -0400

    C - malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,49 +8,51 @@
  * @c: char to assign
  * Description: creat array of size size and assign char c
  * Return: pointer to array, NULL if fail
- *void simple_print_buffer(char *buffer, unsigned int size)
- *{
- *    unsigned int i;
- *
- *    i = 0;
- *    while (i < size)
- *    {
- *        if (i % 10)
- *        {
- *            printf(" ");
- *        }
- *        if (!(i % 10) && i)
- *        {
- *            printf("\n");
- *        }
- *        printf("0x%02x", buffer[i]);
- *        i++;
- *    }
- *    printf("\n");
- *}
- *
- *int main(void)
- *{
- *    char *buffer;
- *
- *    buffer = create_array(98, 'H');
- *    if  (buffer == NULL)
- *    {
- *        printf("failed to allocate memory\n");
- *        return (1);
- *    }
- *    simple_print_buffer(buffer, 98);
- *    free(buffer);
- *    return (0);
- *}
- */
+*
+* 
+* void simple_print_buffer(char *buffer, unsigned int size)
+* {
+* 	unsigned int i;
+* 
+*	i = 0;
+*	while (i < size)
+*	{
+*		if (i % 10)
+*		{
+*			printf(" ");
+*		}
+*		if (!(i % 10) && i)
+*		{
+*			printf("\n");
+*		}
+*		printf("0x%02x", buffer[i]);
+*		i++;
+*	}
+*	printf("\n");
+*}
+*
+*int main(void)
+*{
+*	char *buffer;
+*
+*	buffer = create_array(98, 'H');
+*	if  (buffer == NULL)
+*	{
+*		printf("failed to allocate memory\n");
+*		return (1);
+*	}
+*	simple_print_buffer(buffer, 98);
+*	free(buffer);
+*	return (0);
+*}
+**/
 
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char* ptr = malloc(sizeof(char) * size);
 	
-	if ((size == NULL) || (ptr == 0))
+	if ((size == 0) || (ptr == 0))
 	{
 		return (NULL);
 	}
