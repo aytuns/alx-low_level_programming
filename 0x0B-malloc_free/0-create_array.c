9af5f1107aa70c9c21f3ec67b7f48a8acd2f98b3
aytuns <ayotunde.jerry@gmail.com>
2023-08-09 00:34:03 -0400

    C - malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -50,12 +50,14 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char* ptr = malloc(sizeof(char) * size);
 	
-	if (size == NULL || ptr == 0)
+	if ((size == NULL) || (ptr == 0))
+	{
 		return (NULL);
-
+	}
 	for (i = 0; i < size; i++)
+	{
 		ptr[i] = c;
-
+	}
 	return (ptr);
 }
 
